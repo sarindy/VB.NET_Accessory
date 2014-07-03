30e9c8ac05a21771d58fc0512a1090a1ed2c0937
sarindy <sarindyo@yahoo.com>
2014-07-03 21:29:29 +0700

    Few lines

--- Accessory/Accessory/Module1.vb
+++ Accessory/Accessory/Module1.vb
@@ -1,6 +1,13 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim date1 As New Date(14, 7, 2)
+        Dim date2 As New Date(14, 6, 2)
+
+        Console.WriteLine(date1.ToShortDateString())
+        Console.WriteLine(date1.ToLongDateString)
+        Console.WriteLine(Date.Compare(date1, date2))
+        Console.ReadLine()
 
     End Sub
 
